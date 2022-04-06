61a92e9f592f0da5073d9a0bac52d0541e4394e5
DAscencioSanchez <diaspm77@gmail.com>
2022-04-06 13:27:58 -0500

    update

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -1,1 +1,31 @@
 // 1. Creacion de un objeto con propiedades
+
+let myCar = new Object(); //creacion de un objeto
+myCar.make = 'Ford'; //guardar valor dentre del objeto creado
+myCar.model = 'Mustang'; 
+myCar.year = 1969; 
+
+console.log(myCar) // imprimir el objeto
+
+// 2. Declaracion de un objeto con variables locales y publicas
+
+const myModule = (() => {
+
+// Variables de contexto local
+    const privateFoo = "Soy un valor privado, solo me usan en este objeto"
+    const privateBar = [1,2,3]
+    const baz = "Soy un valor que va a ser expuesto"
+// Variables para guardar variables locales 
+    const exported = {
+        publicFoo : "Valor publico, pueden verme desde donde me llamen",
+        publicBar : "Otro valor publico",
+        publicBaz : baz
+    }
+
+//Exposicion de variables locales
+return exported
+
+
+
+})()
+console.log(myModule)
---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,16 @@
+//logger.js
+
+//Esta es una funcion que se guardara en este modulo como info
+
+exports.info = (message) => {
+    console.log(`info: ${message}`)
+}
+
+//Esta es una funcion que se guardara en este modulo como verbose
+exports.verbose = (message) => {
+    console.log(`verbose: ${message}`)
+}
+
+
+
+
