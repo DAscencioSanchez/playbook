7b27584b5b3333007503c3ba7d04544473e04035
DAscencioSanchez <diaspm77@gmail.com>
2022-04-20 19:46:21 -0500

    script update

--- weekly_mission_2/content/05-unit_tests/pokemon.test.js
+++ weekly_mission_2/content/05-unit_tests/pokemon.test.js
@@ -2,5 +2,5 @@ import Pokemon from './pokemon.js'
 
 test('1) Create a new object pokemon', () => {
     const myPokemon = new Pokemon('Pikachu','electric', 10)
-    expect(myPokemon.name).toBe('Pikachu'); // Corrige esta prueba
+    expect(myPokemon.name).toBe('Pikachusss'); // Corrige esta prueba
 });
